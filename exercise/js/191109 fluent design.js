559be3d35f2700717af4d857d5e5e32798c19a34
illustra9527 <yabesoube@gmail.com>
2019-11-09 20:48:19 +0800

    upload Fluent design file
    上傳 microsoft fluent design仿刻檔案 - 目前進度: 完成 nav bar

---
+++ exercise/js/191109 fluent design.js
@@ -0,0 +1,7 @@
+let toggleMenu = document.querySelector('.toggleMenu')
+let toggleBtn = document.querySelector('.toggleMenuBtn')
+
+
+toggleBtn.onclick = function(){
+    toggleMenu.classList.toggle('active')
+}
