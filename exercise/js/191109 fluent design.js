c95b7c26f0f36ca43be94302766b1dce5cc15a85
illustra9527 <yabesoube@gmail.com>
2019-11-27 01:25:43 +0800

    1127 fluent design 更新
    1. nav及section_1 container 顯示錯誤(加中權重)
    2 section_2 container 及RWD設定

--- exercise/js/191109 fluent design.js
+++ exercise/js/191109 fluent design.js
@@ -5,3 +5,19 @@ let toggleBtn = document.querySelector('.toggleMenuBtn')
 toggleBtn.onclick = function(){
     toggleMenu.classList.toggle('active')
 }
+
+let section_4_window = document.querySelector('.section_4_content_window')
+
+function section_4_window_move(){
+    const window_position = (section_4_window.offsetTop) + (section_4_window.clientHeight / 2);
+    
+    if (window.screenY > window_position){
+        section_4_window.classList.add('visible')
+    }else{
+        section_4_window.classList.remove('visible')
+    }
+}
+
+window.addEventListener('scroll', section_4_window_move);
+
+
